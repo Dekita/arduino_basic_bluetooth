27303d93c5be1d2474b167808a91f603d77db7dc
DekitaRPG <dekitarpg@gmail.com>
2019-05-05 05:00:22 +0100

    Add files via upload

---
+++ basic_bluetooth.ino
@@ -0,0 +1,33 @@
+#include <AltSoftSerial.h>
+
+// Digital pin 8: RX goes to Bluetooth device' TX pin 
+// Digital pin 9: TX goes to Bluetooth device' RX pin
+
+AltSoftSerial altSerial;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Starting:..");
+  altSerial.begin(9600);
+  altSerial.println("Hello World");
+}
+
+void loop() {
+  char c;
+  if (Serial.available()) {
+    c = Serial.read();
+    altSerial.print(c);
+  }
+  if (altSerial.available()) {
+    c = altSerial.read();
+    switch (c) {
+      case '1': digitalWrite(LED_BUILTIN, HIGH); break;
+      case '2': digitalWrite(LED_BUILTIN, LOW);  break;
+    }
+    altSerial.print(c);
+    Serial.print(c);
+  }
+}
+
+
